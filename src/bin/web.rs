afd1d7def4da9758c85b260301b40813673500a0
Andrey <idevtier@gmail.com>
2024-12-30 14:14:24 +0100

    add: State to the web app

--- src/bin/web.rs
+++ src/bin/web.rs
@@ -1,6 +1,11 @@
+use promptpunch::{llm::chat_gpt::ChatGpt, web::AppState};
+
 #[tokio::main]
 async fn main() {
-    let router = promptpunch::web::init_router();
+    let state = AppState {
+        llm: ChatGpt::from_env(),
+    };
+    let router = promptpunch::web::init_router().with_state(state);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, router).await.unwrap()
 }
--- src/web.rs
+++ src/web.rs
@@ -1,8 +1,22 @@
+use crate::llm::chat_gpt::ChatGpt;
 use askama_axum::Template;
-use axum::{response::IntoResponse, routing::get, Router};
+use axum::{
+    extract::{Form, State},
+    response::IntoResponse,
+    routing::{get, post},
+    Router,
+};
+use std::collections::HashMap;
 
-pub fn init_router() -> Router {
-    Router::new().route("/", get(get::root))
+#[derive(Clone)]
+pub struct AppState {
+    pub llm: ChatGpt,
+}
+
+pub fn init_router() -> Router<AppState> {
+    Router::new()
+        .route("/", get(get::root))
+        .route("/", post(post::generate))
 }
 
 mod get {
@@ -14,5 +28,17 @@ mod get {
 
     pub async fn root() -> impl IntoResponse {
         Root {}
+    }
+}
+
+mod post {
+    use super::*;
+
+    pub async fn generate(
+        State(_state): State<AppState>,
+        Form(form): Form<HashMap<String, String>>,
+    ) -> impl IntoResponse {
+        tracing::info!("Got form request {form:?}");
+        "Generated prompt"
     }
 }
