e259de3bd33b2928e5083f6f466868dd51533cba
Andrey <idevtier@gmail.com>
2024-12-30 14:30:19 +0100

    add: Tracing subscriber

--- src/bin/web.rs
+++ src/bin/web.rs
@@ -1,11 +1,17 @@
 use promptpunch::{llm::chat_gpt::ChatGpt, web::AppState};
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
     let state = AppState {
         llm: ChatGpt::from_env(),
     };
     let router = promptpunch::web::init_router().with_state(state);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    tracing::info!("Starting app");
     axum::serve(listener, router).await.unwrap()
 }
