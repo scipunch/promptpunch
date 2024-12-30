179841411870b23293ca81383ca867b58c697aed
Andrey <idevtier@gmail.com>
2024-12-30 15:13:31 +0100

    add: Error handling

--- src/web.rs
+++ src/web.rs
@@ -2,6 +2,7 @@ use crate::llm::chat_gpt::ChatGpt;
 use askama_axum::Template;
 use axum::{
     extract::{Form, State},
+    http::StatusCode,
     response::IntoResponse,
     routing::{get, post},
     Router,
@@ -42,12 +43,33 @@ mod post {
         placeholder_value: String,
     }
 
+    #[derive(Template)]
+    #[template(path = "error.html")]
+    struct PromptPunchError {
+        message: String,
+    }
+
     pub async fn generate(
         State(_state): State<AppState>,
         Form(req): Form<GenerateRequest>,
     ) -> impl IntoResponse {
         tracing::info!("Got form request {req:?}");
+        let GenerateRequest {
+            prompt,
+            placeholder_name,
+            placeholder_value,
+        } = req;
+
+        if !prompt.contains(&placeholder_name) {
+            return (
+                StatusCode::BAD_REQUEST,
+                PromptPunchError {
+                    message: format!("Placeholder {} not found in the prompt", placeholder_name),
+                },
+            )
+                .into_response();
+        }
         tokio::time::sleep(std::time::Duration::from_secs(3)).await;
-        "Generated prompt"
+        "Generated prompt".into_response()
     }
 }
